6f8a2aaf87511bc9e3032819a0d73f737d12cd5f
Yunus ÖLMEZ <yns4832@gmail.com>
2024-08-31 03:25:39 +0200

    sample data sharing

--- js-implementation/detail/src/index.js
+++ js-implementation/detail/src/index.js
@@ -1,3 +1,15 @@
 console.log("Detail page is running");
+import data from "../../db/data.json";
 
-document.getElementById("dev-detail").innerHTML = "Detail Page";
+let detailText = "";
+const id = Number(getParameter("item_id"));
+if (id) {
+  detailText += `<p>${data.list.find((i) => i.id == id).detail}<p>`;
+
+  document.getElementById("dev-detail").innerHTML = detailText;
+}
+
+function getParameter(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
--- js-implementation/list/src/index.js
+++ js-implementation/list/src/index.js
@@ -1,4 +1,11 @@
-let appName = "List Application is running";
-console.log(appName);
+import data from "../../db/data.json";
 
-document.getElementById("dev-list").innerHTML = appName;
+let appName = "List Application is running";
+
+let html = "<ul>";
+for (const d of data.list) {
+  html += `<li onclick="javascript:window.location.href = '?item_id=${d.id}'">${d.title} </li> `;
+}
+html += "</ul>";
+
+document.getElementById("dev-list").innerHTML = appName + html;
